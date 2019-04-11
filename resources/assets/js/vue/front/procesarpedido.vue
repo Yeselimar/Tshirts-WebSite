4dd07fe991ad85c8a3d4b8e58056d493b2367dc1
Rafael Delgado <rafael1delgado@hotmail.com>
2019-04-11 10:45:41 -0400

    Ajustes en los estilos

---
+++ resources/assets/js/vue/front/procesarpedido.vue
@@ -0,0 +1,13 @@
+<script>
+import procesarpedidoComponent from "../../components/procesarpedidoComponent.vue";
+
+const app = new Vue({
+  el: "#procesarPedido",
+  name:'procesarPedido',
+  data: {
+  },
+  components: {
+    procesarpedidoComponent
+    }
+});
+</script>
