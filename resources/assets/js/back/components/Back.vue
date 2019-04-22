92c43a6dc274c11a24bad185da30c17149b9159f
Rafael Delgado <rafael1delgado@hotmail.com>
2019-04-22 16:26:13 -0400

    Añadiendo el admin y sus css

--- resources/assets/js/back/components/Back.vue
+++ resources/assets/js/back/components/Back.vue
@@ -1,17 +1,10 @@
 <template>
     <div>
-        <v-app>
-
         <router-view></router-view>
-        </v-app>
-
     </div>
 </template>
 <script>
-import 'vuetify/dist/vuetify.min.css'
-import '@mdi/font/css/materialdesignicons.css'
 
-import 'chartist/dist/chartist.min.css'
 
 export default {
     created(){
