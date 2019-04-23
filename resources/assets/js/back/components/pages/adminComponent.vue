06de6efe02fc0459213bfc65f4e000341f728c5c
Rafael Delgado <rafael1delgado@hotmail.com>
2019-04-22 21:59:59 -0400

    Ajustes pequeños

--- resources/assets/js/back/components/pages/adminComponent.vue
+++ resources/assets/js/back/components/pages/adminComponent.vue
@@ -193,7 +193,7 @@
                                     <li><a href="#"><i class="ti-wallet"></i> Balance</a></li>
                                     <li><a href="#"><i class="ti-email"></i> Inbox</a></li>
                                     <li><a href="#"><i class="ti-settings"></i> Setting</a></li>
-                                    <li><a href="#"><i class="fa fa-power-off"></i> Logout</a></li>
+                                    <li><a href="#" @click="logout"><i class="fa fa-power-off"></i> Logout</a></li>
                                 </ul>
                             </div>
                         </li>
@@ -738,7 +738,7 @@ export default {
     return {
       message: 'Hoera!!!!'
     }
-  }, methods: {
+  },methods: {
   	logout (){
 	  	CerService.post("/logout/admin")
 	          .then(response => {
